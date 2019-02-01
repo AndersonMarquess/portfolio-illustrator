2154808f906ce01bdc3d9d3666f862388348c793
AndersonMarquess <andersongabriel96.ag@gmail.com>
2019-01-31 22:36:38 -0200

    Criado efeito para o viewer

--- js/img_viewer.js
+++ js/img_viewer.js
@@ -1,8 +1,34 @@
+img_class = "";
 
 function showFullImage(img) {
     var elementIMG = document.getElementById("img-viewer-id");
-    elementIMG.classList.add(img);
+    if(elementIMG.classList.length >= 2) {
+        return;
+    }
 
+    elementIMG.classList.add(img);
+    openEffect();
+    
+    img_class = img;
+}
+
+function closeViewer() {
+    document.getElementById("img-viewer-id").classList.remove(img_class);
+    closeEffect();
+}
+
+function openEffect() {
     var viewContainer = document.getElementById("figure-main-viewer");
+    viewContainer.classList.add("show-viewer");
+    viewContainer.classList.remove("close-viewer");
     viewContainer.style.display = "block";
+    document.getElementById("viewer-icon").style.display = "block";
+}
+
+function closeEffect() {
+    var viewContainer = document.getElementById("figure-main-viewer");
+    viewContainer.classList.remove("show-viewer");
+    viewContainer.classList.add("close-viewer");
+    viewContainer.style.display = "none";
+    document.getElementById("viewer-icon").style.display = "none";
 }
